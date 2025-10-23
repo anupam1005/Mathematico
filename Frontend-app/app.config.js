add8449021a1d94d8421adffeec9ada66823cb4d
anupam1005 <anupam1005@example.com>
2025-10-23 13:40:47 +0530

    fix version code

--- Frontend-app/app.config.js
+++ Frontend-app/app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'mathematico-app',
     version: '2.0.0',
     orientation: 'portrait',
-    owner: 'testaccount125',
+    owner: 'testaccount0515',
     icon: './assets/icon.png',
     newArchEnabled: false,
     sdkVersion: '54.0.0',
@@ -15,25 +15,24 @@ export default {
     },
     extra: {
       eas: {
-        projectId: 'f4a7320d-9731-4ddb-9e3f-a8c056934cd9'
+        projectId: '6bc3167a-315b-4be1-9b1b-8398fdc59921'
       }
     },
     android: {
       package: 'com.anupam1505.mathematicoapp',
-      versionCode: 4,
+      versionCode: 5,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff'
       },
       permissions: []
     },
-    plugins: [
+    plugins: IS_DEV ? [
       'expo-secure-store',
       'expo-dev-client'
+    ] : [
+      'expo-secure-store'
     ],
-    developmentClient: {
-      silentLaunch: true
-    },
     // Add platform-specific configurations
     platforms: ['ios', 'android', 'web'],
     // Ensure proper module resolution
