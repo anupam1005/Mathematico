f6a77e82943abbff904e137db24fb485f16533a7
anupam1005 <anupam1005@example.com>
2026-01-02 19:57:31 +0530

    update some files

--- Frontend-app/app.config.js
+++ Frontend-app/app.config.js
@@ -25,47 +25,32 @@ export default {
       },
       permissions: []
     },
-    plugins: IS_DEV ? [
-      'expo-secure-store',
-      'expo-dev-client',
-      'expo-font',
-      './plugins/withRazorpay.js',
+    plugins: [
       [
         'expo-build-properties',
         {
           android: {
-            compileSdkVersion: 35,
-            targetSdkVersion: 35,
+            compileSdkVersion: 34,
+            targetSdkVersion: 34,
+            buildToolsVersion: '34.0.0',
             minSdkVersion: 24,
+            kotlinVersion: '1.8.0',
             enable16KbPageSize: true,
             packagingOptions: {
               jniLibs: {
                 useLegacyPackaging: false
               }
             }
+          },
+          ios: {
+            useFrameworks: 'static'
           }
         }
-      ]
-    ] : [
+      ],
       'expo-secure-store',
       'expo-font',
       './plugins/withRazorpay.js',
-      [
-        'expo-build-properties',
-        {
-          android: {
-            compileSdkVersion: 35,
-            targetSdkVersion: 35,
-            minSdkVersion: 24,
-            enable16KbPageSize: true,
-            packagingOptions: {
-              jniLibs: {
-                useLegacyPackaging: false
-              }
-            }
-          }
-        }
-      ]
+      ...(IS_DEV ? ['expo-dev-client'] : [])
     ]
   }
 };
