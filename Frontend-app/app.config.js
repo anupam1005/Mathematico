6e66bd33ed6dea6757143c3e15dccb36a6d4d45c
anupam1005 <anupam1005@example.com>
2026-02-15 02:44:16 +0530

    Update version code for Internal Testing

--- Frontend-app/app.config.js
+++ Frontend-app/app.config.js
@@ -22,7 +22,7 @@ export default {
     },
     android: {
       package: 'com.anupam1505.mathematicoapp',
-      versionCode: 14,
+      versionCode: 15,
       usesCleartextTraffic: false,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
