10630900893f57285a227d13de428e723da31cd9
anupam1005 <anupam1005@example.com>
2026-02-22 17:31:39 +0530

    update

--- Frontend-app/app.config.js
+++ Frontend-app/app.config.js
@@ -22,7 +22,7 @@ export default {
     },
     android: {
       package: 'com.anupam1505.mathematicoapp',
-      versionCode: 22,
+      versionCode: 23,
       usesCleartextTraffic: false,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
