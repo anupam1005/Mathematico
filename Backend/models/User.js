4e55c814f0b3637153760dc4260689b9c371957f
anupam1005 <anupam1005@example.com>
2026-03-02 20:03:45 +0530

    fix

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -26,7 +26,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email is required'],
-    unique: true,
     lowercase: true,
     trim: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
