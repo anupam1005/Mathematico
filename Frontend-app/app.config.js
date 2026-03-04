87f221fcc82414dd9685e8e2e348137ae1920b29
anupam1005 <anupam1005@example.com>
2026-03-04 21:09:16 +0530

    update

--- Frontend-app/app.config.js
+++ Frontend-app/app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: 'Mathematico',
     slug: 'mathematico-app',
-    version: '8.4.4',
+    version: '8.4.5',
     jsEngine: 'hermes',
     orientation: 'portrait',
     owner: 'dipanjan_mathematico',
@@ -24,7 +24,7 @@ export default {
     },
     android: {
       package: 'com.anupam1505.mathematicoapp',
-      versionCode: 30,
+      versionCode: 31,
       usesCleartextTraffic: false,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
